c4ad218e4905b46f77bd45c0ab55607a790c98de
AmirAini <amirhaini@gmail.com>
2023-04-18 14:20:46 +0800

    add genre controller

--- app/Command/Movie/GenreController.php
+++ app/Command/Movie/GenreController.php
@@ -1,12 +1,11 @@
 <?php
 
-
 namespace App\Command\Movie;
 
 use Database\movieSeeder;
 use Minicli\CommandController;
 
-class IndexController extends CommandController
+class GenreController extends CommandController
 {
     protected $movieSeeder;
 
@@ -17,18 +16,26 @@ class IndexController extends CommandController
 
     public function handle()
     {
+        $genre = $this->hasParam('filter') ? ucfirst($this->getParam('filter')) : 'Action';
+
         //connect to db
         $conn = $this->movieSeeder->tableConnection();
 
-        $query = "SELECT * FROM imdb_movies ORDER BY RAND() LIMIT 300";
+        $stmt = $conn->prepare("SELECT * FROM imdb_movies WHERE genre LIKE CONCAT('%', ?, '%') ORDER BY RAND() LIMIT 200");
 
-        $result = $conn->query($query);
+        $stmt->bind_param("s", $genre);
+
+        if (!$stmt->execute()) {
+            die("Execute failed: " . $stmt->error);
+        }
+
+        $result = $stmt->get_result();
+
+
         if (!$result) {
             die("Query failed: " . $conn->error);
         }
 
         $this->getPrinter()->displayRecords($result);
-
-        $conn->close();
     }
 }
